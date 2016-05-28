88fe0bac7aa908305961b64e11b535043c0bbbe9
weiyu.lin <weiyu.in@rakuten.com>
2016-05-28 13:23:15 +0800

    cleanup codes and add comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,45 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {Uri, Position, CompletionList, Disposable, CompletionItem, CompletionItemKind} from 'vscode';
 var path = require('path');
 var _ = require('lodash');
 var fetch = require('node-fetch');
 var parseString = require('xml2js').parseString;
 var disposableProvider;
 var isProviderEnabled = false;
-// this method is called when your extension is activated
-export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "google-complete" is now active!');
+/**
+ *  Activate Function
+ */
+export function activate(context: vscode.ExtensionContext) {
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+    /**
+     *  googleCompleteMe Command Function
+     */
     let disposableCommand = vscode.commands.registerTextEditorCommand('extension.googleCompleteMe', (textEditor: any, edit) => {
 
         if (isProviderEnabled) {
+            
             isProviderEnabled = false;
             vscode.window.setStatusBarMessage('Google Complete Me is disabled');
-            Disposable.from(disposableProvider).dispose();
+            vscode.Disposable.from(disposableProvider).dispose();
 
         } else {
             isProviderEnabled = true;
             vscode.window.setStatusBarMessage('Google Complete Me is enabled');
             var completeProvider = new CompleteProvider();
             disposableProvider = vscode.languages.registerCompletionItemProvider('*',
-                completeProvider, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z");
+                completeProvider, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z');
 
             let document = textEditor.document;
             let filepath = document.fileName;
             let firstSelectEnd = textEditor._selections[0].end;
             let uri = document.uri;
-            let position = new Position(firstSelectEnd.line, firstSelectEnd.character);
+            let position = new vscode.Position(firstSelectEnd.line, firstSelectEnd.character);
 
-            vscode.commands.executeCommand('vscode.executeCompletionItemProvider', uri, position).then((result: CompletionItem[]) => {
-
-            });
-
+            vscode.commands.executeCommand('vscode.executeCompletionItemProvider', uri, position).then((result: vscode.CompletionItem[]) => {});
 
         }
 
@@ -64,7 +61,6 @@ class CompleteProvider implements vscode.CompletionItemProvider {
 
         return new Promise((resolve, reject) => {
             var userQuery = getUserKeyIn(lineText, position);
-
             fetch('http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + getUserKeyIn(lineText, position))
                 .then(function (res) {
                     return res.text();
@@ -72,16 +68,18 @@ class CompleteProvider implements vscode.CompletionItemProvider {
                     parseString(body, function (err, result) {
 
                         if (err) {
+                            //Case 1. API Not Available
                             vscode.window.showInformationMessage('Google Autocomplte API is not available for the query');
                             reject(err);
                         } else {
-                            if (typeof _.get(result, "toplevel.CompleteSuggestion") === "undefined") {
+                            //Case 2. API Has No Suggestion Word
+                            if (typeof _.get(result, 'toplevel.CompleteSuggestion') === 'undefined') {
                                 reject();
                             } else {
+                                //Case 3. API Has Suggestions
                                 var items = result.toplevel.CompleteSuggestion.map((item) => {
-                                    return new vscode.CompletionItem(item.suggestion[0]["$"].data);
+                                    return new vscode.CompletionItem(item.suggestion[0]['$'].data);
                                 });
-                                console.log(items);
                                 resolve(items);
                             }
 
@@ -96,7 +94,7 @@ class CompleteProvider implements vscode.CompletionItemProvider {
 }
 
 function getUserKeyIn(lineText: string, position: vscode.Position): string {
-    let temp = lineText.substr(0, position.character).trim().split(" ");
+    let temp = lineText.substr(0, position.character).trim().split(' ');
     return temp[temp.length - 1];
 }
 
